c807c55d62f0bcfc2b62169cd85e8ad7351c1aa9
apoterenko <a.poterenko@gmail.com>
2016-09-22 19:50:35 +0300

    *add angular2 tests

--- src/MetadataHelper.spec.ts
+++ src/MetadataHelper.spec.ts
@@ -1,4 +1,6 @@
-import {PropertyAnnotationFactory} from './MetadataFactory';
+import {Input, Output} from '@angular/core';
+
+import {PropertyAnnotationFactory, IAnnotation} from './MetadataFactory';
 
 import {
     MetadataHelper,
@@ -11,7 +13,6 @@ describe('MetadataHelper', ()=> {
         it('MetadataHelper should work correctly', ()=> {
 
             class ViewFieldMetadata {
-
                 configValue:number;
 
                 constructor(config) {
@@ -19,7 +20,7 @@ describe('MetadataHelper', ()=> {
                 }
             }
 
-            const ViewField = PropertyAnnotationFactory(ViewFieldMetadata);
+            const ViewField:IAnnotation = PropertyAnnotationFactory(ViewFieldMetadata);
 
             class View {
                 @ViewField({configValue: 100})
@@ -27,14 +28,24 @@ describe('MetadataHelper', ()=> {
 
                 @ViewField({configValue: 200})
                 private field2:number;
+
+                @Input()
+                private field3:number;
+
+                @Input()
+                private field4:number;
+
+                @Output()
+                private field5:number;
             }
 
             const viewInstance:View = new View();
 
             expect(Object.keys(MetadataHelper.findPropertyMetadata(viewInstance, ViewField))).toEqual(['field1', 'field2']);
+            expect(Object.keys(MetadataHelper.findPropertyMetadata(viewInstance, Input))).toEqual(['field3', 'field4']);
+            expect(Object.keys(MetadataHelper.findPropertyMetadata(viewInstance, Output))).toEqual(['field5']);
 
             const annotationMetadataHolder:IAnnotationMetadataHolder = MetadataHelper.findPropertyMetadata(viewInstance, ViewField);
-
             expect(annotationMetadataHolder['field1']['configValue']).toBe(100);
             expect(annotationMetadataHolder['field2']['configValue']).toBe(200);
         });
